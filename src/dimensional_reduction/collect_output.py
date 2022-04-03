57476a2a7c222a08de7a4362004c50a79709b8d6
Tianyu Dai <td115@duke.edu>
2022-04-03 10:37:57 -0400

    add another observable

--- src/dimensional_reduction/collect_output.py
+++ src/dimensional_reduction/collect_output.py
@@ -6,8 +6,11 @@ n_dp = range(1, 25, 1)
 output = []
 
 for i_dp in n_dp: 
-    data = np.loadtxt("../../data/dp_output/RAA_dp%d" %i_dp)
-    i_output = data.T[1]
+    data_1 = np.loadtxt("../../data/dp_output/centrality0-10/RAA_dp%d" %i_dp)
+    data_2 = np.loadtxt("../../data/dp_output/centrality40-50/RAA_dp%d" %i_dp)
+    i_output_1 = data_1.T[1]
+    i_output_2 = data_2.T[1]
+    i_output = np.concatenate((i_output_1, i_output_2), axis=0)
     output.append(i_output)
 
 np.savetxt('../../data/dp_output/output', output)
